006c9f745da3072c4dfcc0c2147e96b1020cb0d7
fred <fred908@hotmail.ca>
2018-02-14 16:12:46 -0500

    Ajout de la creation des commandes, des plats, de la liste de clients et la verification de la conformite des commandes

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,84 @@
 import java.io.*;
-import java.nio.charset.Charset;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Main {
-	
-	public static void main( String[] args ) {
+
+	public static void main(String[] args) {
 
 		String ligne;
 		BufferedReader ficLecture;
 		Path chemin = Paths.get("commandesAPasser.txt");
-		
+
 		String cheminAbsolu = chemin.toAbsolutePath().toString();
-		
+
 		System.out.println(cheminAbsolu);
-		
-		try { 
-			
+
+		ArrayList<String> clients = new ArrayList<String>();
+		Hashtable<String, Plats> plats = new Hashtable<String, Plats>();
+		ArrayList<Commandes> commandes = new ArrayList<Commandes>();
+
+		try {
+
 			ficLecture = new BufferedReader(new FileReader(cheminAbsolu));
-			
+
 			ligne = ficLecture.readLine();
-					while ( !( ligne = ficLecture.readLine() ).equals("Plats :")) {
-						System.out.println(ligne);
+			while (!(ligne = ficLecture.readLine()).equals("Plats :")) {
+
+				clients.add(ligne);
+				System.out.println(ligne);
+
+			}
+
+			while (!(ligne = ficLecture.readLine()).equals("Commandes :")) {
+
+				String[] ligneDiviser = ligne.split(" ");
+				plats.put(ligneDiviser[0], new Plats(ligneDiviser[0], Double.parseDouble(ligneDiviser[1])));
+
+				System.out.println(ligne);
+
+			}
+			while (!(ligne = ficLecture.readLine()).equals("Fin")) {
+
+				String[] ligneDiviser = ligne.split(" ");
+
+				if (ligneDiviser.length == 3) {
+
+					try {
+						
+						if (!clients.contains(ligneDiviser[0])) {
+							throw new Exception("Client non existant.");
+						}
+						
+						commandes.add(new Commandes(ligneDiviser[0], plats.get(ligneDiviser[1]),
+								Integer.parseInt(ligneDiviser[2])));
+						
+					} catch (Exception e) {
+						
+						ficLecture.close();
+						throw new Exception("Le fichier ne respecte pas le format demandé !");
 						
 					}
-					
-					while ( !( ligne = ficLecture.readLine() ).equals("Commandes :")) {
-						System.out.println(ligne);
-					}
-					while ( !( ligne = ficLecture.readLine() ).equals("Fin")) {
-						System.out.println(ligne);
-					}
-			
-		} 
-		catch ( IOException err) {
-			
-			System.out.println(err);
-		}
-		
 
-		
-		
+				} else {
+					
+					ficLecture.close();
+					throw new Exception("Le fichier ne respecte pas le format demandé !");
+
+				}
+
+				System.out.println(ligne);
+			}
+
+			ficLecture.close();
+
+		} catch (Exception err) {
+
+			System.out.println(err);
+
+		}
+
 	}
-	
-	
+
 }
